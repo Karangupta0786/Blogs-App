af71374ee44cf0493c70b3d50b86db935b426dc5
Karan <karansuggu@gmail.com>
2024-06-16 22:46:33 +0530

    000

--- src/main/java/com/example/Blog/App/BlogAppApplication.java
+++ src/main/java/com/example/Blog/App/BlogAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class
-BlogAppApplication {
+public class BlogAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApplication.class, args);
